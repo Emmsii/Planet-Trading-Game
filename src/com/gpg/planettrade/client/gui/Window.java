7b4955f58913230663b4612f1930b5dd599f52b0
Tamfoolery <tamfoolery@live.co.uk>
2014-12-18 12:51:10 +0000

    + Text Renderer + Font * Update & Render loops run better

--- src/com/gpg/planettrade/client/gui/Window.java
+++ src/com/gpg/planettrade/client/gui/Window.java
@@ -12,8 +12,6 @@ public class Window {
 	protected final int w, h;
 	protected final String title;
 
-	private boolean windowOpen = false;
-
 	public Window(int w, int h, String title)
 	{
 		this.w = w;
@@ -23,51 +21,98 @@ public class Window {
 
 	public void start(GUI gui)
 	{
-		try {
-			Display.setDisplayMode(new DisplayMode(w, h));
-			Display.setTitle(title);
-			Display.create(new PixelFormat(8, 8, 0, 8)); // Anti-aliasing ([Alpha bits], [Depth bits], [Stencil bits], [Samples]) (8xAA)
-			windowOpen = true;
-		}
-		catch (LWJGLException e)
-		{
-			windowOpen = false;
-			e.printStackTrace();
-			System.exit(0);
-		}
-
 		initGL();
+
+		gui.initLoop();
+
+		double delta = 1;
+		double nextTime = (double) System.nanoTime() / 1000000000.0;
+		double maxTimeDiff = 0.5;
+		int skippedFrames = 1;
+		int maxSkippedFrames = 5;
 
 		while (!Display.isCloseRequested())
 		{
-			windowOpen = true;
+			double currTime = (double) System.nanoTime() / 1000000000.0;
+			if ((currTime - nextTime) > maxTimeDiff) nextTime = currTime;
+			if (currTime >= nextTime)
+			{
+				nextTime += delta;
+				gui.update();
 
-			glClear(GL_COLOR_BUFFER_BIT);
+				if ((currTime < nextTime) || (skippedFrames > maxSkippedFrames))
+				{
+					if (Display.isActive() && Display.isVisible())
+					{
+						glClear(GL_COLOR_BUFFER_BIT);
+						glLoadIdentity();
+						gui.render();
+						skippedFrames = 1;
+					}
+				}
+				else
+				{
+					skippedFrames++;
+				}
+			}
+			else
+			{
+				int sleepTime = (int)(1000.0 * (nextTime - currTime));
 
-			gui.render();
+				if (sleepTime > 0)
+				{
+					try
+					{
+						Thread.sleep(sleepTime);
+					}
+					catch (InterruptedException e)
+					{
+						e.printStackTrace();
+					}
+				}
+			}
 
 			Display.update();
 		}
 
-		windowOpen = false;
 		Display.destroy();
 	}
 
-	private static void initGL()
+	private void initGL()
 	{
+		try {
+			Display.setDisplayMode(new DisplayMode(w, h));
+			Display.setTitle(title);
+			Display.create(new PixelFormat(8, 8, 0, 8)); // Anti-aliasing ([Alpha bits], [Depth bits], [Stencil bits], [Samples]) (8xAA)
+		}
+		catch (LWJGLException e)
+		{
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		glEnable(GL_TEXTURE_2D);
+		glShadeModel(GL_SMOOTH);
+		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_LIGHTING);
+
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClearDepth(1);
+
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		glViewport(0, 0, w, h);
+		glMatrixMode(GL_MODELVIEW);
+
 		glMatrixMode(GL_PROJECTION); // Select Projection mode as current matrix
 		glLoadIdentity(); // Clear current matrix
-		glOrtho(0, Display.getWidth(), 0, Display.getHeight(), -1, 1); // Set to orthographic view, covering the screen, in 2D (rendering anything from z-1 to z1)
+		glOrtho(0, Display.getWidth(), Display.getHeight(), 0, -1, 1); // Set to orthographic view, covering the screen, in 2D (rendering anything from z-1 to z1)
 		glMatrixMode(GL_MODELVIEW); // Return to model view matrix
 
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set clear colour to black
 
 		glDisable(GL_DEPTH_TEST); // Disable the depth test because we're in 2D
-	}
-
-	public boolean isOpen()
-	{
-		return windowOpen;
 	}
 
 }
--- src/com/gpg/planettrade/client/gui/gui.java
+++ src/com/gpg/planettrade/client/gui/gui.java
@@ -2,6 +2,9 @@ package com.gpg.planettrade.client.gui;
 
 public interface GUI {
 
+	public void initLoop();
+
+	public void update();
 	public void render();
 
 }
