e8fc9fa6e68bb52efb57b9a9bbd1459ba8f80eef
Tamfoolery <tamfoolery@live.co.uk>
2014-12-21 00:16:32 +0000

    -_-

--- src/com/gpg/planettrade/clientnew/states/State.java
+++
@@ -1,9 +0,0 @@
-//package com.gpg.planettrade.clientnew.states;
-//
-//import com.gpg.planettrade.clientnew.gui.FontRenderer;
-//
-//public abstract class State {
-//
-//	protected FontRenderer font = new FontRenderer();
-//
-//}
